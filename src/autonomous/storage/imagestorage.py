f507cbcf3fd92661e87944700e548d142d20f9f5
StevenMoore <stevenallenmoorer@gmail.com>
2023-12-31 17:52:15 +0000

    updated

--- src/autonomous/storage/imagestorage.py
+++ src/autonomous/storage/imagestorage.py
@@ -18,7 +18,7 @@ class ImageStorage:
     def _get_key(cls, folder="", pkey=None):
         if folder and not folder.endswith("/"):
             folder = f"{folder}/"
-        folder = f"{folder.replace('/', '.')}{pkey or uuid.uuid4()}."
+        folder = f"{folder.replace('/', '.')}{pkey or uuid.uuid4()}"
         return f"{folder}"
 
     def _resize_image(self, asset_id, max_size):
@@ -58,6 +58,7 @@ class ImageStorage:
             log(f"Original image not found: {original_path}")
             return ""
         file_path = f"{original_path}/{size}.webp"
+        #log(file_path)
         if size != "orig" and not os.path.exists(file_path):
             # If the file doesn't exist, create it
             result = self._resize_image(asset_id, size)
@@ -68,7 +69,7 @@ class ImageStorage:
             if not full_url
             else f"{os.environ.get('APP_BASE_URL', '')}/{file_path}"
         )
-        log(f"Returning image url: {result_url}")
+        #log(f"Returning image url: {result_url}")
         return result_url
 
     def get_path(self, asset_id):
@@ -80,18 +81,18 @@ class ImageStorage:
         else:
             return self.base_path
 
-    def search(self, folder="", size="orig", **kwargs):
+    def search(self, folder="", **kwargs):
         imgs = []
+        #log(f"{self.base_path}")
         for f in os.listdir(f"{self.base_path}/{folder}"):
+            #log(f"{self.base_path}/{folder}/{f}")
             for img in os.listdir(f"{self.base_path}/{folder}/{f}"):
-                if size in img:
-                    img_key = self._get_key(
-                        f"{folder}",
-                        pkey=f,
-                    )
-                    # log(img_key)
-                    imgs.append(img_key)
-
+                img_key = self._get_key(
+                    f"{folder}",
+                    pkey=f,
+                )
+                imgs.append(img_key)
+        #log(imgs)
         return imgs
 
     def remove(self, asset_id):
